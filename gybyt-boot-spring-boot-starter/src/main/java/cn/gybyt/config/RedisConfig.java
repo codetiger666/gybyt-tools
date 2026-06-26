f3d1172d44c0d700b75f6400c66aa3b6c5c8ef0e
codetiger <codetiger806@gmail.com>
2026-06-26 14:38:54 +0800

    fix: mybatis sql log print

--- gybyt-boot-spring-boot-starter/src/main/java/cn/gybyt/config/RedisConfig.java
+++ gybyt-boot-spring-boot-starter/src/main/java/cn/gybyt/config/RedisConfig.java
@@ -1,5 +1,10 @@
 package cn.gybyt.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -33,22 +38,30 @@ public class RedisConfig {
 
     /**
      * 注入RedisTemplate对象
+     *
      * @param factory
      * @return
      */
     @Bean
     public RedisTemplate<String, Object> gybytRedisTemplate(RedisConnectionFactory factory) {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL,
+                                 JsonTypeInfo.As.PROPERTY);
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(om));
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(om));
         template.afterPropertiesSet();
         return template;
     }
+
     /**
      * 注入RedisTemplate对象
+     *
      * @param factory
      * @return
      */
@@ -66,13 +79,24 @@ public class RedisConfig {
 
     /**
      * 注入RedisCacheManage对象
+     *
      * @param redisTemplate
      * @return
      */
     @Bean
-    public CacheManager gybytRedisCacheManager(@Qualifier(value = "gybytRedisTemplate") RedisTemplate<String, Object> redisTemplate) {
-        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(Objects.requireNonNull(redisTemplate.getConnectionFactory()));
-        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())).entryTtl(Duration.ofHours(1L));
+    public CacheManager gybytRedisCacheManager(
+            @Qualifier(value = "gybytRedisTemplate") RedisTemplate<String, Object> redisTemplate) {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL,
+                                 JsonTypeInfo.As.PROPERTY);
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(
+                Objects.requireNonNull(redisTemplate.getConnectionFactory()));
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                        new GenericJackson2JsonRedisSerializer(om)))
+                .entryTtl(Duration.ofHours(1L));
         return new RedisCacheManager(redisCacheWriter, defaultCacheConfig);
     }
 
--- gybyt-boot-spring-boot-starter/src/main/java/cn/gybyt/interceptor/GybytMybatisSqlLogInterceptor.java
+++ gybyt-boot-spring-boot-starter/src/main/java/cn/gybyt/interceptor/GybytMybatisSqlLogInterceptor.java
@@ -3,17 +3,17 @@ package cn.gybyt.interceptor;
 import cn.gybyt.config.properties.GybytMybatisProperties;
 import cn.gybyt.util.BaseUtil;
 import cn.gybyt.util.ReflectUtil;
-import org.apache.ibatis.executor.statement.PreparedStatementHandler;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
 import org.apache.ibatis.session.ResultHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Proxy;
 import java.sql.Statement;
 import java.util.Map;
 import java.util.Objects;
@@ -27,19 +27,13 @@ import java.util.regex.Pattern;
  *
  * @author codetiger
  */
-@Intercepts({
-        @Signature(type = StatementHandler.class, method = "query", args = {Statement.class, ResultHandler.class}),
-        @Signature(type = StatementHandler.class, method = "update", args = Statement.class),
-        @Signature(type = StatementHandler.class, method = "batch", args = Statement.class)
-})
+@Intercepts({@Signature(type = StatementHandler.class, method = "query", args = {Statement.class, ResultHandler.class}), @Signature(type = StatementHandler.class, method = "update", args = Statement.class), @Signature(type = StatementHandler.class, method = "batch", args = Statement.class)})
 public class GybytMybatisSqlLogInterceptor implements Interceptor {
 
     private final Logger log = LoggerFactory.getLogger(GybytMybatisSqlLogInterceptor.class);
-    private final static String DRUID_POOL_CLASS_NAME = "com.alibaba.druid.pool.DruidPooledPreparedStatement";
-    private final static String SEATA_DATASORCE_PROXY_CLASS_NAME = "io.seata.rm.datasource.PreparedStatementProxy";
     private final Pattern sqlPattern;
     private GybytMybatisProperties gybytMybatisProperties;
-    private static final Map<String, Pattern> PATTERN_MAP = new ConcurrentHashMap<>();
+    private final static Map<String, Pattern> PATTERN_MAP = new ConcurrentHashMap<>();
 
     public GybytMybatisSqlLogInterceptor(GybytMybatisProperties gybytMybatisProperties) {
         this.gybytMybatisProperties = gybytMybatisProperties;
@@ -49,100 +43,76 @@ public class GybytMybatisSqlLogInterceptor implements Interceptor {
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        Object firstArg = invocation.getArgs()[0];
-        Statement statement;
-        // 代理对象获取代理对象，不是获取原对象
-        if (Proxy.isProxyClass(firstArg.getClass())) {
-            statement = ReflectUtil.getFieldValueByFieldName(firstArg, "h.statement");
-        } else {
-            statement = (Statement) firstArg;
-        }
-        // 阿里巴巴连接池特殊处理
-        if (ReflectUtil.isSameType(DRUID_POOL_CLASS_NAME, ReflectUtil.getClass(statement))) {
-            Statement statement1 = ReflectUtil.getFieldValueByFieldName(statement, "stmt.raw");
-            statement = statement1 == null ? statement : statement1;
-        }
-        // SEATA分布式事务特殊处理
-        if (ReflectUtil.isSameType(SEATA_DATASORCE_PROXY_CLASS_NAME, ReflectUtil.getClass(statement))) {
-            Statement statement1 = ReflectUtil.getFieldValueByFieldName(statement, "targetStatement.delegate");
-            statement = statement1 == null ? statement : statement1;
-        }
-        String sql = statement.toString();
-        // 格式化sql语句
-        if (BaseUtil.isNotEmpty(sql)) {
-            try {
-                sql = sql.replaceAll("\\s+", " ");
-                Matcher matcher = sqlPattern.matcher(sql);
-                matcher.find();
-                sql = matcher.group(1);
-            } catch (Exception ignored) {
-            }
-        }
-        // 获取实际sql执行方法
+        // 获取实际 StatementHandler
         Object target = invocation.getTarget();
-        Object delegate;
-        if (Proxy.isProxyClass(target.getClass())) {
-            delegate = ReflectUtil.getFieldValueByFieldName(target, "h.target.delegate");
-        } else {
-            delegate = ReflectUtil.getFieldValueByFieldName(target, "delegate");
-        }
-        MappedStatement mappedStatement = ReflectUtil.getFieldValueByFieldName(delegate, "mappedStatement");
+        StatementHandler statementHandler = (StatementHandler) target;
         // 计算执行 SQL 耗时
         long start = System.currentTimeMillis();
         Object result = invocation.proceed();
         long end = System.currentTimeMillis();
-        if (BaseUtil.isNotEmpty(sql)) {
-            try {
-                if (sql.contains("oracle.jdbc.driver")) {
-                    BoundSql boundSql = ((PreparedStatementHandler) delegate).getBoundSql();
-                    sql = boundSql.getSql();
-                    Object parameterObject = boundSql.getParameterObject();
-                    if (BaseUtil.isSimpleType(parameterObject)) {
-                        sql = sql.replaceFirst("\\?", toStr(parameterObject));
-                    } else {
-                        for (ParameterMapping parameterMapping : boundSql.getParameterMappings()) {
-                            String property = parameterMapping.getProperty();
-                            if (BaseUtil.isEmpty(property)) {
-                                sql = sql.replaceFirst("\\?", toStr(boundSql.getParameterObject()));
-                                continue;
-                            }
-                            String[] propertyArray = property.split("\\.");
-                            Object value = parameterMapping;
-                            int index = 1;
-                            for (String key : propertyArray) {
-                                if (index == 1) {
-                                    value = getData(parameterObject, key);
-                                } else {
-                                    value = getData(value, key);
-                                }
-                                index++;
-                            }
-                            sql = sql.replaceFirst("\\?", toStr(value));
+        // 获取 MappedStatement
+        MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
+        String sql = "";
+        try {
+            BoundSql boundSql = statementHandler.getBoundSql();
+            sql = boundSql.getSql();
+            Object parameterObject = boundSql.getParameterObject();
+            if (BaseUtil.isSimpleType(parameterObject)) {
+                sql = sql.replaceFirst("\\?", toStr(parameterObject));
+            } else {
+                for (ParameterMapping parameterMapping : boundSql.getParameterMappings()) {
+                    try {
+                        String property = parameterMapping.getProperty();
+                        if (BaseUtil.isEmpty(property)) {
+                            sql = sql.replaceFirst("\\?", toStr(boundSql.getParameterObject()));
+                            continue;
                         }
+                        String[] propertyArray = property.split("\\.");
+                        Object value = parameterMapping;
+                        int index = 1;
+                        for (String key : propertyArray) {
+                            if (index == 1) {
+                                if (boundSql.hasAdditionalParameter(key)) {
+                                    value = boundSql.getAdditionalParameter(key);
+                                } else {
+                                    value = getData(parameterObject, key);
+                                }
+                            } else {
+                                value = getData(value, key);
+                            }
+                            index++;
+                        }
+                        sql = sql.replaceFirst("\\?", toStr(value));
+                    } catch (Exception ignored) {
+                        log.error("sql处理失败", ignored);
                     }
-                    sql = sql.replaceAll("\\s+", " ");
-                    Matcher matcher = sqlPattern.matcher(sql);
-                    matcher.find();
-                    sql = matcher.group(1);
-                }
-            } catch (Exception e) {
-                log.error("sql处理失败", e);
-            }
-            String executeId = BaseUtil.isNotEmpty(mappedStatement) ? Objects.requireNonNull(mappedStatement)
-                                                                             .getId() : "";
-            // 处理跳过的包
-            for (String skipPackage : gybytMybatisProperties.getSkipPackages()) {
-                if (getPattern(skipPackage).matcher(executeId).find()) {
-                    return result;
                 }
             }
-            log.info(
-                    "\n\n==============  Sql Start  ==============\nExecute ID  ：{}\nExecute SQL ：{}\nExecute Time：{} ms\n==============  Sql  End   ==============\n",
-                    executeId,
-                    sql,
-                    end - start
-            );
+            sql = sql.replaceAll("\\s+", " ");
+            Matcher matcher = sqlPattern.matcher(sql);
+            matcher.find();
+            sql = matcher.group(1);
+        } catch (Exception e) {
+            log.error("sql处理失败", e);
         }
+        MappedStatement mappedStatement = null;
+        if (metaObject.hasGetter("delegate.mappedStatement")) {
+            mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+        } else if (metaObject.hasGetter("mappedStatement")) {
+            mappedStatement = (MappedStatement) metaObject.getValue("mappedStatement");
+        }
+        String executeId = BaseUtil.isNotEmpty(mappedStatement) ? Objects.requireNonNull(mappedStatement)
+                .getId() : "";
+        // 处理跳过的包
+        for (String skipPackage : gybytMybatisProperties.getSkipPackages()) {
+            if (getPattern(skipPackage).matcher(executeId)
+                    .find()) {
+                return result;
+            }
+        }
+        log.info(
+                "\n\n==============  Sql Start  ==============\nExecute ID  ：{}\nExecute SQL ：{}\nExecute Time：{} ms\n==============  Sql  End   ==============\n",
+                executeId, sql, end - start);
         return result;
     }
 
@@ -178,7 +148,7 @@ public class GybytMybatisSqlLogInterceptor implements Interceptor {
 
     private Object getData(Object o, String key) {
         if (o instanceof Map) {
-           return  ((Map) o).get(key);
+            return ((Map) o).get(key);
         }
         return ReflectUtil.getFieldValueByFieldName(o, key);
     }
@@ -187,7 +157,8 @@ public class GybytMybatisSqlLogInterceptor implements Interceptor {
         if (o == null) {
             return "";
         }
-        switch (o.getClass().getSimpleName()) {
+        switch (o.getClass()
+                .getSimpleName()) {
             case "String":
                 return BaseUtil.format("'{}'", o);
             case "Date":
